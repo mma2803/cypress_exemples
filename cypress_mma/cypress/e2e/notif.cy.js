ccb79ae02fea38851387726b6ecf9577be548d01
REDA <reda@Mac.lan>
2024-12-27 21:43:13 +0100

    MAJ POSTMAN

--- cypress_mma/cypress/e2e/notif.cy.js
+++ cypress_mma/cypress/e2e/notif.cy.js
@@ -1,20 +1,16 @@
 let username = "Dina20"
 let password = "s3cret"
 let contact = "Ruthie Prosacco"
-let montant = "5"
-let montantEnReel =`$${montant}.00`
-const { faker } = require('@faker-js/faker');
-// or, if desiring a different locale
-// const { fakerDE: faker } = require('@faker-js/faker');
-const firstname = faker.person.firstName();
-const lastname = faker.person.lastName(); 
-
+let montant = "10"
+let montantEnReel = `$${montant}.00`
+let numberNotificationsIcon
 let motif = "Achats"
+
 describe('All about notifications', () => {
     beforeEach(() => {
         cy.login(username, password);
     });
-    it('affichage page notif', () => {
+    /*it('affichage page notif', () => {
         cy.get('[data-test="sidenav-notifications"]').should('contain', 'Notifications').click()
         cy.get('main h2')
             .should('be.visible')
@@ -34,6 +30,24 @@ describe('All about notifications', () => {
             .should('contain','requested')
             .should('contain',montantEnReel)
             
+    })*/
+    it('je vérifie si le nbr de notifications affichées identique aux nbr de lignes ', () => {
+        let countLinesNumber
+        let notifcationsBadgeNumber
+        cy.get('[data-test="sidenav-notifications"]').should('contain', 'Notifications').click()
+        cy.get('ul[data-test="notifications-list"]').find('li button').first().click({force:true})
+        cy.get('ul[data-test="notifications-list"]').find('li').its('length').then((count) => {
+            cy.log(`Nombre total de notifications - lignes: ${count}`);
+            //countLinesNumber = count
+            cy.get('[data-test="nav-top-notifications-count"],[data-testid="NotificationsIcon"]').siblings('span').invoke('text').then((badgeValue) => {
+                cy.log(`Valeur du badge : ${badgeValue}`);
+                //notifcationsBadgeNumber = +badgeValue
+            cy.wrap(count).should('equal', +badgeValue); 
+                
+                
+            })
+        })
+        
     })
 })
 
--- cypress_mma/cypress/e2e/request_money.cy.js
+++ cypress_mma/cypress/e2e/request_money.cy.js
@@ -12,12 +12,12 @@ montant_char = `+$${montant}.00`
 
 describe('request_money', () => {
   beforeEach(() => {
-    cy.login(username, password, myname);
+    cy.login(username, password);
   });
   it('devrait permettre d\'envoyer de l\'argent', () => {
-    cy.visit("http://localhost:3000/transaction/new")
+    //cy.visit("http://localhost:3000/transaction/new")
     cy.log(montant_char);
-    //cy.get('a[href="/transaction/new"]').click();
+    cy.get('a[href="/transaction/new"]').click();
     cy.get('[data-test="users-list"]').should("contain", contact).click();
     cy.get('input[placeholder="Amount"]').type(montant);
     cy.get('input[placeholder="Add a note"]').type(motif);
